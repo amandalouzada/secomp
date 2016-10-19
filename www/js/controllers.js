238bd0b186cb61d611268669bcd624387b0d591e
Amanda Louzada <amandanuneslouzada@gmail.com>
2016-10-19 17:16:22 -0200

    cronograma dinamico

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,43 +1,71 @@
-angular.module('starter.controllers', [])
+  angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-})
+  .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  })
 
-.controller('CronogramaCtrl', function($scope) {
-  $scope.cronogramas= [{
-      id:0,
-      dia:'18/10',
-      evento: [{
-        palestra: 'Amanda',
+  .controller('CronogramaCtrl', function($scope) {
+    $scope.dia=0;
+    $scope.cronogramas= [{
+        id:0,
+        dia:'18/10',
+        evento: [{
+          palestra: 'Ionic Framework',
 
 
-        horario:'14:00 - 18:00'
-      }]
-  },{
-      id:1,
-      dia:'19/10',
-      evento: [{
-        palestra: 'Warley',
-        horario:'14:00 - 18:00'
+          horario:'14:00 - 18:00'
+        },{
+            palestra: 'Haxes Multiliguagem',
+
+
+            horario:'14:00 - 18:00'
+          }],
+    },{
+        id:1,
+        dia:'19/10',
+        evento: [{
+          palestra: 'Estrutura de Dados',
+          horario:'14:00'
+        },{
+          palestra: 'Mineração de dados',
+          horario:'16:00'
+        },{
+          palestra: 'Patrick Leitouze',
+          horario:'18:00'
+        }],
       },{
-        palestra: 'Ary',
-        horario:'18:00 - 20:00'
-      }]
-  }];
+          id:2,
+          dia:'20/10',
+          evento: [{
+            palestra: 'Redes',
+            horario:'14:00'
+          },{
+            palestra: 'SIG',
+            horario:'16:00'
+          },{
+            palestra: 'Ionic Framework',
+            horario:'18:00'
+          }]
+    }];
 
-})
 
-.controller('PalestranteCtrl', function($scope, $ionicPopup) {
-  $scope.exibirAmanda = function() {
-    $ionicPopup.alert({
-      title: 'Ionic Framework',
-      template: 'Minicurso ministrado no lab 15'
-    });
-  }
-  $scope.exibirWarley = function() {
-    $ionicPopup.alert({
-      title: 'Minic curso do Warley',
-      template: 'Minicurso ministrado no lab xyz'
-    });
-  }
-});
+
+    $scope.definirDia = function(id){
+      $scope.dia = id;
+    }
+
+  })
+
+  .controller('PalestranteCtrl', function($scope, $ionicPopup) {
+    $scope.exibirAmanda = function() {
+      $ionicPopup.alert({
+        title: 'Ionic Framework',
+        template: 'Minicurso ministrado no lab 15'
+      });
+    }
+    $scope.exibirWarley = function() {
+      $ionicPopup.alert({
+        title: 'Minic curso do Warley',
+        template: 'Minicurso ministrado no lab xyz'
+      });
+    }
+  });
